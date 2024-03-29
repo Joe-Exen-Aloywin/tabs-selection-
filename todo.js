29ff0541ce29c12ddb745990b6151bdb618ca23b
Joe-Exen-Aloywin <162951568+Joe-Exen-Aloywin@users.noreply.github.com>
2024-03-29 19:16:56 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,10 @@
+        function addTask() {
+            var taskInput = document.getElementById("taskInput");
+            var taskText = taskInput.value.trim();
+            if (taskText !== "") {
+                var li = document.createElement("li");
+                li.textContent = taskText;
+                document.getElementById("taskList").appendChild(li);
+                taskInput.value = "";
+            }
+        }
